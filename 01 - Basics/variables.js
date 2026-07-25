452dc354d98887b1bfaf0329d31f56b5fd69d925
caiomatheus-dev <caiomatheus.dev@gmail.com>
2026-07-24 21:56:12 -0300

    Variable let and var

---
+++ 01 - Basics/variables.js
@@ -0,0 +1,8 @@
+let nome; // declarou a variavel mas não inciou 
+nome = "Caio"; // Aqui acontece a inicialização da variavel declarada acima !
+console.log(nome);
+nome = "Matheus"; // alterando o valor da variavel crida acima 
+console.log(nome); // e reexibindo aqui 
+
+
+
